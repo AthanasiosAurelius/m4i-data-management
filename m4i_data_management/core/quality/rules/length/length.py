12a9631721eefa26e9e44c4d5268b1c2e59cc038
Thijs Franck <thijs.franck@aureliusenterprise.com>
2022-12-12 15:24:10 +0100

    updated length metric

--- m4i_data_management/core/quality/rules/length/length.py
+++ m4i_data_management/core/quality/rules/length/length.py
@@ -5,17 +5,16 @@ def length(data: DataFrame, column_name: str, required_length: int) -> Series:
     """
     Checks if the number of characters of the values in the column with the given `column_name` are equal to the `required_length`. 
 
-    This only works for textual values.
-    If a value is not a string, it is converted to a string before comparison.
+    This function only works for array-like values such as strings or lists.
     
-    If the length is equal, or if the value is empty, assigns a score of 1. 
-    Otherwise, assigns a score of 0.
+    If the length of a value is equal or greater than the `required_length`, assigns a score of 1. 
+    Otherwise, or if the value is empty, assigns a score of 0.
     """
 
     def check(value):
 
-        if isna(value):
-            return 1
+        if not isinstance(value, list) and isna(value):
+            return 0
         # END IF
 
         has_required_length = (
--- m4i_data_management/core/quality/rules/length/test__length.py
+++ m4i_data_management/core/quality/rules/length/test__length.py
@@ -8,28 +8,42 @@ def test__length_with_equal_length():
 
     data = DataFrame([
         {
-            "id": 1234
+            "id": "1234"
         }
     ])
 
     result = length(data, "id", 4)
 
     assert result.sum() == 1
-# END test__length_with_equal_length_single_value
+# END test__length_with_equal_length
 
 
-def test__length_with_non_equal_length():
+def test__length_with_greater_length():
 
     data = DataFrame([
         {
-            "id": 12345
+            "id": "123456"
+        }
+    ])
+
+    result = length(data, "id", 4)
+
+    assert result.sum() == 1
+# END test__length_with_equal_length
+
+
+def test__length_with_smaller_length():
+
+    data = DataFrame([
+        {
+            "id": "12"
         }
     ])
 
     result = length(data, "id", 4)
 
     assert result.sum() == 0
-# END test__length_with_non_equal_length_single_value
+# END test__length_with_smaller_length
 
 
 def test__length_without_value():
@@ -42,5 +56,33 @@ def test__length_without_value():
 
     result = length(data, "id", 4)
 
+    assert result.sum() == 0
+# END test__length_without_value
+
+
+def test__length_with_list():
+
+    data = DataFrame([
+        {
+            "id": [1, 2, 3, 4]
+        }
+    ])
+
+    result = length(data, "id", 4)
+
     assert result.sum() == 1
+# END test__length_without_value
+
+
+def test__length_with_empty_list():
+
+    data = DataFrame([
+        {
+            "id": []
+        }
+    ])
+
+    result = length(data, "id", 4)
+
+    assert result.sum() == 0
 # END test__length_without_value
