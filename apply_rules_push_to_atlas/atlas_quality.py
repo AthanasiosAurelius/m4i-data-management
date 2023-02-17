cb01a825d00cbe4184e3b1f9d2e8d694bef987dd
Athanasios Giokaris <athanasios.giokaris@outlook.com>
2023-02-17 10:58:37 +0100

    Update atlas_quality.py

--- apply_rules_push_to_atlas/atlas_quality.py
+++ apply_rules_push_to_atlas/atlas_quality.py
@@ -23,7 +23,7 @@ data = DataFrame(columns=columns_names)
 print(data)
 
 
-
+#atlas quality rules
 store = ConfigStore.get_instance()
 
 atlas_dataset_quality = Quality(
@@ -38,6 +38,29 @@ atlas_dataset_quality = Quality(
 
 print(atlas_dataset_quality)
 
+
+#use this to provide a dataset for rules
+def atlas_get_quality_rules_dataset():
+    """
+    A dataframe with all the data quality rules in atlas for a dataset given in the config
+
+    :param dataSet_name: The name of the data set we are checking (example: for fte its
+    :return: data - DataFrame of the qualifed Rules and the information pretaining to it as
+    """
+
+    dataset_entity = get_entity_by_guid(store.get("atlas_dataset_guid"))
+
+    rule_dataframe = atlas_get_quality_rules_empty_dataframe()
+
+    for field in dataset_entity["entity"]["attributes"]["fields"]:
+        field_entity = get_entity_by_guid(field["guid"])
+        for rule in field_entity["entity"]["relationshipAttributes"]["dataQuality"]:
+            rule_entity = get_entity_by_guid(rule["guid"])
+            rule_dataframe = rule_dataframe.append(
+                atlas_create_data_quality_rule_data_dictionary_representation(rule_entity))
+    rule_dataframe = rule_dataframe.infer_objects()
+    return rule_dataframe.set_index(keys="id", drop=False)
+# END atlas_get_quality_rules_dataset
 
 
 
