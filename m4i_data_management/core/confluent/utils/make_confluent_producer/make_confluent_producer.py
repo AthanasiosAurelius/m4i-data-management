eb7991e2cac04ceb9b793fe365af40cd000a861d
Gabriella Pascha <gabriella.pascha@gmail.com>
2023-03-09 20:54:44 +0100

    propogate to kafka

--- m4i_data_management/core/confluent/utils/make_confluent_producer/make_confluent_producer.py
+++ m4i_data_management/core/confluent/utils/make_confluent_producer/make_confluent_producer.py
@@ -8,21 +8,29 @@ config = ConfigStore.get_instance()
 def make_confluent_producer() -> Producer:
     """
     Returns a connection to Confluent Kafka in the Data Management Platform that can be used to push change events.
+
+
     """
+    sasl_flag = config.get('sasl_flag', default=True)
+    bootstrap_server_url = config.get("confluent.kafka.bootstrap.servers")
 
-    bootstrap_server_url, username, password = config.get_many(
-        "confluent.kafka.bootstrap.servers",
-        "confluent.auth.sasl.username",
-        "confluent.auth.sasl.password"
-    )
+    if sasl_flag:
+        username, password = config.get_many(
+            "confluent.auth.sasl.username",
+            "confluent.auth.sasl.password"
+        )
 
-    producer_config = {
-        "bootstrap.servers": bootstrap_server_url,
-        "sasl.mechanisms": "PLAIN",
-        "sasl.password": password,
-        "sasl.username": username,
-        "security.protocol": "SASL_SSL"
-    }
+        producer_config = {
+            "bootstrap.servers": bootstrap_server_url,
+            "sasl.mechanisms": "PLAIN",
+            "sasl.password": password,
+            "sasl.username": username,
+            "security.protocol": "SASL_SSL"
+        }
+    else:
+        producer_config = {
+            "bootstrap.servers": bootstrap_server_url,
+        }
 
     return Producer(**producer_config)
 # END make_confluent_producer
