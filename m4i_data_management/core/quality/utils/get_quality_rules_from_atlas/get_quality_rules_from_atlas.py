743cd5183f870aed9600cc05d9717bc9d5157f08
thijsfranck <thijsfranck@gmail.com>
2023-03-10 08:25:54 +0100

    applied asyncio

--- m4i_data_management/core/quality/utils/get_quality_rules_from_atlas/get_quality_rules_from_atlas.py
+++ m4i_data_management/core/quality/utils/get_quality_rules_from_atlas/get_quality_rules_from_atlas.py
@@ -1,12 +1,13 @@
-from pandas import DataFrame
-from m4i_atlas_core import get_entities_by_attribute, ConfigStore, get_entity_by_guid, Entity
 import asyncio
-from typing import Optional
+from typing import List, Optional
 
-from config import config
+from m4i_atlas_core import (BusinessDataQuality, BusinessDataset,
+                            BusinessField, ConfigStore,
+                            get_entities_by_attribute, get_entity_by_guid)
+from pandas import DataFrame
 
 
-def atlas_get_quality_rules_empty_dataframe():
+def atlas_get_quality_rules_dataframe(data: List[BusinessDataQuality] = []):
     """
     :return:  Empty DataFrame with column names as expected for data quality rules based of the data dictionary
     """
@@ -21,14 +22,13 @@ def atlas_get_quality_rules_empty_dataframe():
         "active",
         "expression_version"
     ]
-    data = DataFrame(columns=columns_names)
+    data = DataFrame(data, columns=columns_names)
 
     return data
-
-
 # END atlas_get_quality_rules_empty_dataframe
 
-def get_data_quality_rule_details(quality_rule_entity: Entity) -> dict:
+
+def get_data_quality_rule_details(quality_rule_entity: BusinessDataQuality) -> dict:
     """
      Create attribute dictionary for the given data-quality-rule
     :param quality_rule_entity: The data quality rule entity
@@ -49,11 +49,61 @@ def get_data_quality_rule_details(quality_rule_entity: Entity) -> dict:
     }
 
     return rule
-
-
 # END get_data_quality_rule_details
 
-def get_quality_rules_from_atlas(dataset_name: str, dataset_atlas_guid: Optional[str] = None) -> DataFrame:
+
+async def get_dataset(dataset_name: str, access_token: str, dataset_atlas_guid: Optional[str] = None) -> BusinessDataset:
+
+    if dataset_atlas_guid is None:
+
+        search_results = await get_entities_by_attribute(
+            attribute_name='name',
+            attribute_value=dataset_name,
+            type_name='m4i_dataset',
+            access_token=access_token
+        )
+
+        dataset_entity_headers = search_results.entities
+
+        if len(dataset_entity_headers) > 1:
+            print(
+                f"More then one dataset with the name '{dataset_name}' was found, please provide the atlas guid of the dataset as well.")
+            exit(1)
+        if len(dataset_entity_headers) == 0:
+            print(
+                f"No dataset entity with qualified name '{dataset_name}' was found. Please provide the atlas guid of the dataset as well.")
+            exit(1)
+
+        dataset_atlas_guid = dataset_entity_headers[0].guid
+
+    return await get_entity_by_guid(guid=dataset_atlas_guid, access_token=access_token)
+# END get_dataset
+
+
+async def get_fields(dataset: BusinessDataset, access_token: str) -> List[BusinessField]:
+
+    fields = [
+        get_entity_by_guid(guid=field.guid, access_token=access_token)
+        for field in dataset.attributes.fields
+    ]
+
+    return await asyncio.gather(*fields)
+# END get_fields
+
+
+async def get_quality_rules(fields: List[BusinessField], access_token: str) -> List[BusinessDataQuality]:
+
+    quality_rules = [
+        get_entity_by_guid(guid=quality_rule.guid, access_token=access_token)
+        for field in fields
+        for quality_rule in field.relationship_attributes['dataQuality']
+    ]
+
+    return await asyncio.gather(*quality_rules)
+# END get_quality_rules 
+
+
+async def get_quality_rules_from_atlas(dataset_name: str, dataset_atlas_guid: Optional[str] = None) -> DataFrame:
     """
     Given a dataset_name or dataset_atlas_guid gets all data quality rules associated to it and return them as a dataframe.
 
@@ -66,32 +116,9 @@ def get_quality_rules_from_atlas(dataset_name: str, dataset_atlas_guid: Optional
 
     access_token = store.get('access_token', default=None)
 
-    if dataset_atlas_guid is None:
-        dataset_entity_headers = asyncio.run(get_entities_by_attribute(attribute_name='name',
-                                                                       attribute_value=dataset_name,
-                                                                       type_name='m4i_dataset',
-                                                                       access_token=access_token)).entities
+    dataset = await get_dataset(dataset_name, access_token, dataset_atlas_guid)
+    fields = await get_fields(dataset, access_token)
+    quality_rules = await get_quality_rules(fields, access_token)
 
-        if len(dataset_entity_headers) > 1:
-            print(f"More then one dataset with the name '{dataset_name}' was found, please provide the atlas guid of the dataset as well.")
-            exit(1)
-        if len(dataset_entity_headers) == 0:
-            print(f"No dataset entity with qualified name '{dataset_name}' was found. Please provide the atlas guid of the dataset as well.")
-            exit(1)
-        dataset_atlas_guid = dataset_entity_headers[0].guid
-
-    dataset_entity = asyncio.run(get_entity_by_guid(guid=dataset_atlas_guid, access_token=access_token))
-
-    rules_df = atlas_get_quality_rules_empty_dataframe()
-    for field in dataset_entity.attributes.unmapped_attributes['fields']:
-        field_guid = field['guid']
-        field_entity = asyncio.run(get_entity_by_guid(guid=field_guid, access_token=access_token))
-
-        for rule in field_entity.relationship_attributes['dataQuality']:
-            rule_guid = rule['guid']
-            rule_entity = asyncio.run(get_entity_by_guid(guid=rule_guid, access_token=access_token))
-            rule_details = get_data_quality_rule_details(rule_entity)
-            rules_df = rules_df.append(rule_details, ignore_index=True)
-
-    return rules_df
-
+    return atlas_get_quality_rules_dataframe(quality_rules)
+# END get_quality_rules_from_atlas
