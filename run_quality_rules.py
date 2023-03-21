319c300347ae1ecc9fb0b2ab1008f40e791b6bb5
Athanasios Giokaris <athanasios.giokaris@outlook.com>
2023-03-21 12:57:47 +0100

    aaa

--- run_quality_rules.py
+++ run_quality_rules.py
@@ -16,65 +16,34 @@ from m4i_atlas_core import get_keycloak_token
 from m4i_data_management.write_data_quality_results import *
 from m4i_data_management.atlas_get_quality_rules import *
 from m4i_data_management.atlas_get_metadata.atlas_get_metadata_dataset import *
-
-
-
-
-
+import os
 from m4i_data_management import atlas_get_metadata,atlas_get_quality_rules,write_data_quality_results
 
+
+
+
+# Read the CSV file into a pandas DataFrame
 
 
 #Load config credentials                                
 store = ConfigStore.get_instance()
 store.load({**config, **credentials})
 
-#I have to see keycloak token
-
-# access_token=get_keycloak_token()
-# async def data_entity():
-#     dataset_entity = await get_entity_by_guid(store.get("atlas_dataset_guid",access_token))
-#     return dataset_entity
-
-#store.load({})
-
-# atlas_url, username, password = store.get_many(
-#         "atlas.server.url",
-#         "atlas.credentials.username",
-#         "atlas.credentials.password"
-#     )
-
-
-# atlas_url, username, password = store.get_many(
-#     "keycloak.server.url",
-#     "keycloak.credentials.username",
-#     "keycloak.credentials.password"
-#     )
 
 
 
-
-#get_elastic_data= retrieve_elastic_data("example_system--example_collection--example_user--uid--1")
-
-
-
-# #use relative path
+#use relative path
 def get_data_csv()->DataFrame:
+    # cwd = os.getcwd()
+    # rel_path = "m4i_data_management\sample_csv\sample_data.csv"
+    # abs_path = os.path.join(cwd, rel_path)
+    # data = pd.read_csv(abs_path, sep=";")
     data=pd.read_csv(r"C:\Users\Thana\OneDrive\Desktop\sample_csv\sample_data.csv", sep=";")
-    print(data)
-    #data=pd.DataFrame(data)
-    # data=data.columns.str.lower()
-    # data=data.set_index(keys="id", drop=False)
+    
+    
     return data
 
     
-# def get_data_csv():
-#     path=pd.read_csv(r"C:\Users\Thana\OneDrive\Desktop\sample_csv\sample1.csv", sep=";")
-#     data = pd.read_csv(path)
-#     if not isinstance(data, pd.DataFrame):
-#         data = pd.DataFrame(data)
-#     data = data.set_index(keys="id", drop=False)
-#     return data    
       
 
 
@@ -86,21 +55,10 @@ atlas_dataset_quality = Quality(
     propagate= write_data_quality_results.write_data_quality_results_csv_file
 )
 
- #propagate=write_data_quality_results.write_data_quality_results
+ 
 asyncio.run(atlas_dataset_quality.run())
 
-# async def run():
-#     run= await  atlas_dataset_quality.run()
-#     return run
 
-# data=pd.read_csv(r"C:\Users\Thana\OneDrive\Desktop\sample_csv\sample1.csv")
-#     ##data=data.set_index("id")
-#     #
-# data=pd.DataFrame(data)
-
-
-
-#print(data)
 
 
 
